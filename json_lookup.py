b305b05d5312673ef90702b30fc55b4d1d922c06
michael <powerm3@tcd.ie>
2018-02-27 20:33:37 +0000

    port 80 and 443 json lookup

--- json_lookup.py
+++ json_lookup.py
@@ -5,6 +5,7 @@ import sys
 import csv
 import time
 import json
+import unicodedata
 from nested_lookup import nested_lookup
 from datetime import datetime
 from pprint import pprint
@@ -27,6 +28,8 @@ inputfile = sys.argv[1]
 
 #if len(sys.argv)==3:
 outputFile = sys.argv[2]
+
+#port = sys.argv[3]
 '''
 if not os.path.isfile(file) or not os.access(file,os.R_OK):
     print"Can't read input file: " + file + " - exiting"
@@ -34,6 +37,8 @@ if not os.path.isfile(file) or not os.access(file,os.R_OK):
 '''
 with open(inputfile,'r') as json_file:
     data = json.load(json_file)
+    #data = ast.literal_eval(json.dumps(data)) #removes unicode character from output
+
 
 #Flatten Json as  would be accessible if indexing a dictionary
 def flatten_json(y):
@@ -53,48 +58,58 @@ def flatten_json(y):
 
     flatten(y)
     return out
+#if port == '443':
+
+try:
+    cache_control =data['data']['http']['response']['headers']['cache_control']
+except:
+    cache_control ='error'
+
+try:
+    expires =data['data']['http']['response']['headers']['expires']
+except:
+    expires ='error'
+
+try:
+    pragma =data['data']['http']['response']['headers']['pragma']
+except:
+    pragma ='error'
 
 try:
     connected =data['error']
-    print(server)
 except:
     connected ='True'
 
 try:
-    domain =data['domain']
-    print(server)
+    domain =data['domain'].encode('utf8')
 except:
     domain ='error'
 
 try:
     server =data['data']['http']['response']['headers']['server']
-    print(server)
 except:
     server ='error'
 
 try:
     status_line =data['data']['http']['response']['status_line']
-    print(status_line)
 except:
     status_line='error'
 
 
 try:
     secure_renegotiation =data['data']['http']['response']['request']['tls_handshake']['server_hello']['secure_renegotiation']
-    print(secure_renegotiation)
 except:
     secure_renegotiation ='error'
 
 try:
     tls_version_name =data['data']['http']['response']['request']['tls_handshake']['server_hello']['version']['name']
-    print(tls_version_name)
 except:
     tls_version_name ='error'
 
 
 try:
     self_signed =data['data']['http']['response']['request']['tls_handshake']['server_certificates']['certificate']['parsed']['signature']['self_signed']
-    print(self_signed)
+
 except:
     self_signed='error'
 
@@ -102,26 +117,28 @@ except:
 
 try:
     certificate_names=data['data']['http']['response']['request']['tls_handshake']['server_certificates']['certificate']['parsed']['names']
-    print(certificate_names)
 except:
     certificate_names ='error names'
 
 
 try:
     browser_trusted =data['data']['http']['response']['request']['tls_handshake']['server_certificates']['validation']['browser_trusted']
-    print(browser_trusted)
+
 except:
     browser_trusted ='error'
 
 try:
     cipher_suite = data['data']['http']['response']['request']['tls_handshake']['server_hello']['cipher_suite']['name']
-    print(cipher_suite)
+
 except:
     cipher_suite ='error'
 
+#else:
+    #print('erroerfdsf')
+
 with open(outputFile, "a") as myfile:
     writer=csv.writer(myfile)
-    writer.writerow([domain,connected,status_line,secure_renegotiation,tls_version_name,self_signed,certificate_names,browser_trusted,cipher_suite])
+    writer.writerow([domain,connected,server,status_line,cache_control,expires,pragma,secure_renegotiation,tls_version_name,self_signed,certificate_names,browser_trusted,cipher_suite])
 
 #Print a flatten version of the json file
 myfile.close()
--- zgrab_domain.sh
+++ zgrab_domain.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-#This script will run with a list of domain names on port 443
+#This script will run with a csv where the first column is domains
 #cat tmp.csv |./zgrab_domain.sh (i.e. pipe file to script)
+#./zgrab_domain.sh file1 file2
+
 
 #Get time stamp
 TIME_STAMP=$(date +%s)
 touch zgrab_domain_port443_$TIME_STAMP.csv
-echo "Domain,Connected,Server,Status Line,Secure Regotitation,TlS Version,Self Signed,Certificate Names,Browser Trusted,Cipher" >> zgrab_domain_port443_$TIME_STAMP.csv
+touch zgrab_domain_port80_$TIME_STAMP.csv
+touch zgrab_domain_summary_$TIME_STAMP.txt
+echo "Domain,Connected,Server,Status Line,Cache Control,Expires,Pragma,Secure Regotitation,TlS Version,Self Signed,Certificate Names,Browser Trusted,Cipher" >> zgrab_domain_port443_$TIME_STAMP.csv
+echo "Domain,Connected,Server,Status Line,Cache Control,Expires,Pragma,Secure Regotitation,TlS Version,Self Signed,Certificate Names,Browser Trusted,Cipher" >> zgrab_domain_port80_$TIME_STAMP.csv
 
 #Outputs of banner grabs will be outputted to /go/src/github.com/zmap/zgrab directory
 while IFS=, read -a b;
@@ -16,11 +21,23 @@ do
   #  (cd ~/go/src/github.com/zmap/zgrab && cat banners.json | jq '.data.http.response.request.tls_handshake.server_certificates.chain[1].raw', 'Stuff'>> summary_TIME_STAMP.csv)
                 #  OR
     ./json_lookup.py ~/go/src/github.com/zmap/zgrab/banners.json zgrab_domain_port443_$TIME_STAMP.csv #will open json_test_with_domain_port443_TIME_STAMP.csv in json_lookup.py and results to this
-    sleep .1
-done #<tmp.csv
+    sleep .2
+done < $1
+
+
+while IFS=, read -a b;
+do
+
+    (cd ~/go/src/github.com/zmap/zgrab && echo "${b[0]}" | ./zgrab --port 80 --http="/" --lookup-domain --output-file=banners.json)
+
+  #  (cd ~/go/src/github.com/zmap/zgrab && cat banners.json | jq '.data.http.response.request.tls_handshake.server_certificates.chain[1].raw', 'Stuff'>> summary_TIME_STAMP.csv)
+                #  OR
+    ./json_lookup.py ~/go/src/github.com/zmap/zgrab/banners.json zgrab_domain_port80_$TIME_STAMP.csv #will open json_test_with_domain_port443_TIME_STAMP.csv in json_lookup.py and results to this
+    sleep .2
+done < $2
 
 #Summary results
-touch zgrab_domain_port443_summary_$TIME_STAMP.txt
-echo 'Ran zgrab_domain.sh at '+ $TIME_STAMP >> zgrab_domain_port443_summary_$TIME_STAMP.txt
-echo "Files created:" >> zgrab_domain_port443_summary_$TIME_STAMP.txt
-echo 'json_test_with_domain_port443_TIME_STAMP.csv' >> zgrab_domain_port443_summary_$TIME_STAMP.txt
+echo 'Ran zgrab_domain.sh at '+ $TIME_STAMP >> zgrab_domain_summary_$TIME_STAMP.txt
+echo "Files created:" >> zgrab_domain_summary_$TIME_STAMP.txt
+echo 'zgrab_domain_port443_TIME_STAMP.csv' >> zgrab_domain_summary_$TIME_STAMP.txt
+echo 'zgrab_domain_port80_TIME_STAMP.csv' >> zgrab_domain_summary_$TIME_STAMP.txt

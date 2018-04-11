a7c12a31a4e12c8a174399d6f7180a3bb13a8a78
michael <powerm3@tcd.ie>
2018-04-11 16:08:26 +0100

    Updated cron jobs

--- zgrab_job_domain_port80.sh
+++ zgrab_job_domain_port80.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Will also specify an out put file to make this easier for the scripts below as this will more than likely
 #cause issues as all scripts are outputting two files
-~/fyp/final-year-project/zgrab_domain_port80.sh ~/fyp/final-year-project/ip_to_dns_1522088570/resolved_ip_to_dns1522088570.csv ~/fyp/final-year-project/zgrab_domain80scan.csv
+#~/fyp/final-year-project/zgrab_domain_port80.sh ~/fyp/final-year-project/ip_to_dns_1522088570/resolved_ip_to_dns1522088570.csv ~/fyp/final-year-project/zgrab_domain80scan.csv
+~/fyp/final-year-project/zgrab_domain_port80.sh ~/fyp/final-year-project/not_alive_domain_80.csv ~/fyp/final-year-project/zgrab_domain80scan.csv
 sed '1d' ~/fyp/final-year-project/zgrab_domain80scan.csv >> ~/fyp/final-year-project/zgrabDomain80ScanNoHeader.csv
 rm -f summary_zgrab_*.txt
